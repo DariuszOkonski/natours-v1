ae08043d9eee3da40032f902ed19f1cb9fc9b400
Dariusz Okoński <darek200180@gmail.com>
2024-01-04 21:06:08 +0100

    add middleware to check body in post request in tourRoutes

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,12 +4,22 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`, 'utf-8')
 );
 
-exports.checkId = (req, res, next, val) => {
+exports.checkID = (req, res, next, val) => {
   console.log(`REMOVE THIS MIDDLEWARE => Tour id is: ${val}`);
   if (isNaN(val * 1) || val * 1 > tours.length) {
     return res.status(404).json({
       status: 'fail',
       message: 'Invalid ID',
+    });
+  }
+  next();
+};
+
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Missing name of price',
     });
   }
   next();
