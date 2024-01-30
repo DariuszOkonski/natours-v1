0f17f9daf08e409fb8b4965f598e5d30a612b237
Dariusz Okoński <darek200180@gmail.com>
2024-01-30 22:16:29 +0100

    db connection

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful!'))
+  .catch(() => console.log('=== DB NOT CONNECTED ==='));
 
 const app = require('./app');
 
