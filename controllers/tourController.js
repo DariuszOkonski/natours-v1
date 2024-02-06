89d541685a71179fe040315308efc3454d9269a8
Dariusz Okoński <darek200180@gmail.com>
2024-02-06 21:52:03 +0100

    add deleteTour

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -60,7 +60,7 @@ exports.updateTour = async (req, res) => {
 
     res.status(200).json({
       status: 'success',
-      data: { tour: tour },
+      data: { tour },
     });
   } catch (err) {
     res.status(404).json({
@@ -70,11 +70,19 @@ exports.updateTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: 'success',
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
 // module.exports = {
