67aefd975fcca48153dd0847f216abf98a636981
Dariusz Okoński <darek200180@gmail.com>
2024-02-07 18:32:58 +0100

    add script for importing and removing data

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,55 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
+const fs = require('fs');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const Tour = require('../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful!'))
+  .catch(() => console.log('=== DB NOT CONNECTED ==='));
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'),
+);
+
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully imported!');
+    process.exit();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data successfully deleted!');
+    process.exit();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
+
+// run this commands
+// node dev-data/data/import-dev-data.js --import
+// node dev-data/data/import-dev-data.js --delete
