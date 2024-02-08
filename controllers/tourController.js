3f25efa51a71f16521a08000f4ba32ea3d2f4420
Dariusz Okoński <darek200180@gmail.com>
2024-02-08 18:18:01 +0100

    add filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,9 +2,24 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // eslint-disable-next-line node/no-unsupported-features/es-syntax
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    const query = await Tour.find(queryObj);
+
+    // const query = await Tour.find()
+    //   .where('duration')
+    //   .equals(req.query.duration)
+    //   .where('difficulty')
+    //   .equals(req.query.difficulty);
+
+    const tours = await query;
+
     res.status(200).json({
       status: 'success',
+      length: tours.length,
       data: { tours },
     });
   } catch (err) {
