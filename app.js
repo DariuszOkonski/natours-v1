abdf1147aac27f12008678611917babeffd0bc2a
Dariusz Okoński <darek200180@gmail.com>
2024-09-08 10:28:58 +0200

    add post route

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+/* eslint-disable prefer-object-spread */
 /* eslint-disable no-console */
 const fs = require('fs');
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`),
@@ -16,6 +18,26 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    // eslint-disable-next-line no-unused-vars
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    },
+  );
 });
 
 const port = 3000;
