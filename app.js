324cb4b4b41b99a7bf5371c5fcf732f32036ca77
Dariusz Okoński <darek200180@gmail.com>
2024-09-10 19:46:25 +0200

    add morgan

--- app.js
+++ app.js
@@ -2,14 +2,17 @@
 /* eslint-disable no-console */
 const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
+
+app.use(morgan('dev'));
 app.use(express.json());
 
-app.use((req, res, next) => {
-  console.log('Hello from the middleware');
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log('Hello from the middleware');
+//   next();
+// });
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
