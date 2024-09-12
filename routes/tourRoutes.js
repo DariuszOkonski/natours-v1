1aafc9b4e0f0d82101bfd1a531314006e2f78e25
Dariusz Okoński <darek200180@gmail.com>
2024-09-12 18:43:19 +0200

    code refactoring

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,15 +1,16 @@
 const express = require('express');
-const {
-  getAllTours,
-  createTour,
-  getTour,
-  updateTour,
-  deleteTour,
-} = require('../controllers/tourController');
+const tourController = require('../controllers/tourController');
 
 const router = express.Router();
 
-router.route('/').get(getAllTours).post(createTour);
-router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
+router
+  .route('/')
+  .get(tourController.getAllTours)
+  .post(tourController.createTour);
+router
+  .route('/:id')
+  .get(tourController.getTour)
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour);
 
 module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,15 +1,16 @@
 const express = require('express');
-const {
-  getAllUsers,
-  createUser,
-  getUser,
-  updateUser,
-  deleteUser,
-} = require('../controllers/userController');
+const userController = require('../controllers/userController');
 
 const routes = express.Router();
 
-routes.route('/').get(getAllUsers).post(createUser);
-routes.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
+routes
+  .route('/')
+  .get(userController.getAllUsers)
+  .post(userController.createUser);
+routes
+  .route('/:id')
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
 
 module.exports = routes;
